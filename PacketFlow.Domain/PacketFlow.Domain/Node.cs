e563834078c2404ad30abb4a5a09f0b35a240b5c
Andrew Kuipers <deimors@gmail.com>
2018-01-26 23:18:01 -0600

    Added NodePosition

--- PacketFlow.Domain/PacketFlow.Domain/Node.cs
+++ PacketFlow.Domain/PacketFlow.Domain/Node.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PacketFlow.Domain
 {
@@ -11,13 +7,49 @@ namespace PacketFlow.Domain
 
 	}
 
+	public class NodePosition : IEquatable<NodePosition>
+	{
+		public NodePosition(float x, float y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		public float X { get; }
+		public float Y { get; }
+
+		public override bool Equals(object obj) 
+			=> Equals(obj as NodePosition);
+
+		public bool Equals(NodePosition other) 
+			=> other != null &&
+				X == other.X &&
+				Y == other.Y;
+
+		public override int GetHashCode()
+		{
+			var hashCode = 1861411795;
+			hashCode = hashCode * -1521134295 + X.GetHashCode();
+			hashCode = hashCode * -1521134295 + Y.GetHashCode();
+			return hashCode;
+		}
+
+		public static bool operator ==(NodePosition position1, NodePosition position2) 
+			=> Equals(position1, position2);
+
+		public static bool operator !=(NodePosition position1, NodePosition position2) 
+			=> !(position1 == position2);
+	}
+
 	public class Node
 	{
 		public NodeIdentifier Id { get; }
+		public NodePosition Position { get; }
 
-		public Node(NodeIdentifier id)
+		public Node(NodeIdentifier id, NodePosition position)
 		{
 			Id = id ?? throw new ArgumentNullException(nameof(id));
+			Position = position ?? throw new ArgumentNullException(nameof(position));
 		}
 	}
 }
