dbd4b36b6a80e951c0e0b29d6425ff2bc849df61
Andrew Kuipers <deimors@gmail.com>
2018-01-26 22:35:03 -0600

    Actor abstractions

---
+++ Assets/Code/Actors/Actor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+using Workshop.Core;
+
+namespace PacketFlow.Actors
+{
+	public abstract class Actor<TEvent, TCommand, TError> : IObservable<TEvent>, IEnqueueCommand<TCommand>, IEnqueueObservableCommand<TCommand, TError>
+	{
+		private readonly CommandQueue<TCommand, TError> _commandQueue;
+
+		private readonly EventHistory<TEvent> _history = new EventHistory<TEvent>();
+
+		public Actor(IObservable<Unit> processQueueTicks)
+		{
+			_commandQueue = new CommandQueue<TCommand, TError>(CommitEvents, OnError);
+
+			processQueueTicks.Subscribe(_ => ProcessQueue());
+		}
+
+		public IDisposable Subscribe(IObserver<TEvent> observer)
+			=> _history.Subscribe(observer);
+
+		void IEnqueueCommand<TCommand>.Enqueue(TCommand command)
+			=> (_commandQueue as IEnqueueCommand<TCommand>).Enqueue(command);
+
+		IObservable<CommandResult<TError>> IEnqueueObservableCommand<TCommand, TError>.ObserveResult(TCommand command)
+			=> (_commandQueue as IEnqueueObservableCommand<TCommand, TError>).ObserveResult(command);
+
+		protected abstract IHandleCommand<TCommand, TError> CommandHandler { get; }
+
+		protected abstract IEnumerable<TEvent> UncommittedEvents { get; }
+
+		protected abstract void MarkCommitted();
+
+		private void ProcessQueue()
+			=> _commandQueue.ProcessQueue(CommandHandler);
+
+		private void CommitEvents()
+		{
+			_history.CommitEvents(UncommittedEvents);
+			
+			MarkCommitted();
+		}
+
+		private void OnError(TError error)
+			=> Debug.LogError($"Error: {error.ToString()}");
+	}
+}
---
+++ Assets/Code/Actors/CommandQueue.cs
@@ -0,0 +1,42 @@
+﻿using Functional.Maybe;
+using System;
+using System.Collections.Concurrent;
+using UnityEngine;
+using Workshop.Core;
+
+namespace PacketFlow.Actors
+{
+	public class CommandQueue<TCommand, TError> : IEnqueueCommand<TCommand>, IEnqueueObservableCommand<TCommand, TError>
+	{
+		private readonly ConcurrentQueue<ICommandQueueItem<TCommand, TError>> _commandQueue = new ConcurrentQueue<ICommandQueueItem<TCommand, TError>>();
+
+		private readonly Action commit;
+		private readonly Action<TError> onError;
+
+		public CommandQueue(Action commit, Action<TError> onError)
+		{
+			this.commit = commit;
+			this.onError = onError;
+		}
+
+		void IEnqueueCommand<TCommand>.Enqueue(TCommand command)
+			=> _commandQueue.Enqueue(new CommandQueueItem<TCommand, TError>(command));
+
+		IObservable<CommandResult<TError>> IEnqueueObservableCommand<TCommand, TError>.ObserveResult(TCommand command)
+		{
+			var queueItem = new ObservableCommandQueueItem<TCommand, TError>(new CommandQueueItem<TCommand, TError>(command));
+			_commandQueue.Enqueue(queueItem);
+			return queueItem;
+		}
+
+		public void ProcessQueue(IHandleCommand<TCommand, TError> commandHandler)
+		{
+			ICommandQueueItem<TCommand, TError> queueItem;
+
+			while (_commandQueue.TryDequeue(out queueItem))
+				queueItem
+					.Process(commandHandler)
+					.Match(onError, commit);
+		}
+	}
+}
