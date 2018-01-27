b803bfff575133adff6f0b70e8bf2f9b56679cc8
Taylor Fox <TaylorF@iqmetrix.com>
2018-01-27 16:00:42 -0600

    another bad commit. woooooo

---
+++ Assets/Code/Processing/TransportCommands.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Code.Processing
+{
+	public class TransportCommands
+	{
+		public class AddNodeCommand
+		{
+			public Guid ID;
+			public float X;
+			public float Y;
+			public int Capacity;
+			public int NodeType;
+		}
+
+		public class LinkNodeCommand
+		{
+			public Guid SourceID;
+			public int SourcePortDirection;
+			public Guid SinkID;
+			public int SinkPortDirection;
+		}
+
+		public class AddPacketCommand
+		{
+			public Guid PacketID;
+			public int Type;
+			public Guid NodeId;
+		}
+
+	}
+}
