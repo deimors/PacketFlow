80b38efca59d1c472a9f491e96b10494d6480f94
Taylor Fox <TaylorF@iqmetrix.com>
2018-01-26 23:38:33 -0600

    Get rid of unecessary logging

--- Assets/Code/KeyPressHandler.cs
+++ Assets/Code/KeyPressHandler.cs
@@ -14,18 +14,15 @@ public class KeyPressHandler : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log("help, I'm starting but I have no idea what I'm doing");
 		client = networkManager.StartClient();
-		Debug.Log("client started successfully");
+		Debug.Log("Client started successfully");
 	}
 	
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		Debug.Log("Updating - Let's send a message!");
 		foreach (var key in Enum.GetValues(typeof(KeyCode)).Cast<KeyCode>().Where(x => Input.GetKeyDown(x)))
 			client.Send(1, new StringMessage(key.ToString()));
-		Debug.Log("I should have sent a message by now");
 	}
 }
