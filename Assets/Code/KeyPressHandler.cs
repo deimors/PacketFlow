67e252c5baec0cc1cf066939c5efa92564469168
Taylor Fox <TaylorF@iqmetrix.com>
2018-01-26 23:37:10 -0600

    Manual logging and hooking up the Network Manager
    So that it works to debug... still might crash and freeze

--- Assets/Code/KeyPressHandler.cs
+++ Assets/Code/KeyPressHandler.cs
@@ -14,14 +14,18 @@ public class KeyPressHandler : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		client = networkManager.StartClient();		
+		Debug.Log("help, I'm starting but I have no idea what I'm doing");
+		client = networkManager.StartClient();
+		Debug.Log("client started successfully");
 	}
 	
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		Debug.Log("Updating - Let's send a message!");
 		foreach (var key in Enum.GetValues(typeof(KeyCode)).Cast<KeyCode>().Where(x => Input.GetKeyDown(x)))
 			client.Send(1, new StringMessage(key.ToString()));
+		Debug.Log("I should have sent a message by now");
 	}
 }
