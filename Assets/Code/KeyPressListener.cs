5f4133261770b930b6cde5c8be7a83aabd57f9fd
Ryan Marcotte <ryanm@iqmetrix.com>
2018-01-26 22:27:32 -0600

    probably should not register handlers every frame....

--- Assets/Code/KeyPressListener.cs
+++ Assets/Code/KeyPressListener.cs
@@ -8,16 +8,16 @@ public class KeyPressListener : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
 		NetworkServer.RegisterHandler(1, networkMessage =>
 		{
 			var message = networkMessage.ReadMessage<StringMessage>();
 			Debug.Log(message.value);
 		});
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
 	}
 }
