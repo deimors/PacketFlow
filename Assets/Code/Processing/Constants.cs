eaec6c90e1dca871510647e200221f91866d4872
Taylor Fox <TaylorF@iqmetrix.com>
2018-01-27 16:23:05 -0600

    command to message mapper and transport objects

--- Assets/Code/Processing/Constants.cs
+++ Assets/Code/Processing/Constants.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PacketFlow.Domain;
+using System;
 using System.Collections.Generic;
 
 namespace Assets.Code
@@ -9,7 +10,7 @@ namespace Assets.Code
 		public const int ADMIN_PLAYER_MESSAGE_TYPE_ID = 881;
 		public const int HACKER_PLAYER_TYPE = 0;
 		public const int ADMIN_PLAYER_TYPE = 1;
-		public const int COMMAND_PAYLOAD_TYPE = 0;
+		public const int ADD_NODE_TRE_PAYLOAD_TYPE = 0;
 		public const int EVENT_PAYLOAD_TYPE = 1;
 
 		public static class TransportNodeType
@@ -19,19 +20,11 @@ namespace Assets.Code
 			public const int Consumer = 2;
 		}
 
-		public static class TransportPortDirection
+		public static class TransportCommandPayloadType
 		{
-			public const int Top = 0;
-			public const int Right = 1;
-			public const int Bottom = 2;
-			public const int Left = 3;
-		}
-
-		public static class TransportPacketType
-		{
-			public const int Red = 0;
-			public const int Blue = 1;
-			public const int Green = 2;
+			public const int AddNode = 0;
+			public const int LinkNode = 1;
+			public const int AddPacket = 2;
 		}
 	}
 }
