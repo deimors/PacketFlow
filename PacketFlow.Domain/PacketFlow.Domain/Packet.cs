5681c542a0d6708b069d194fae85529fcfa44002
Andrew Kuipers <deimors@gmail.com>
2018-01-27 10:20:16 -0600

    Defined Packet entity

---
+++ PacketFlow.Domain/PacketFlow.Domain/Packet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PacketFlow.Domain
+{
+	public class PacketIdentifier { }
+
+	public enum PacketType { Red, Blue, Green }
+
+	public class Packet
+	{
+		public Packet(PacketIdentifier id, PacketType type)
+		{
+			Id = id ?? throw new ArgumentNullException(nameof(id));
+			Type = type;
+		}
+
+		public PacketIdentifier Id { get; }
+		public PacketType Type { get; }
+	}
+}
