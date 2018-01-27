b22d46fee3441542f4a4a96374ed1c7ff0f5f4e0
Ryan Marcotte <ryanm@iqmetrix.com>
2018-01-26 22:32:52 -0600

    message queue

--- Assets/Code/KeyPressListener.cs
+++ Assets/Code/KeyPressListener.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -6,18 +7,27 @@ using UnityEngine.Networking.NetworkSystem;
 
 public class KeyPressListener : MonoBehaviour {
 
+	private readonly ConcurrentQueue<StringMessage> _messageQueue = new ConcurrentQueue<StringMessage>();
+
 	// Use this for initialization
 	void Start () {
 		NetworkServer.RegisterHandler(1, networkMessage =>
 		{
 			var message = networkMessage.ReadMessage<StringMessage>();
-			Debug.Log(message.value);
+			_messageQueue.Enqueue(message);
 		});
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		
+		while (!_messageQueue.IsEmpty)
+		{
+			StringMessage stringMessage;
+			if (_messageQueue.TryDequeue(out stringMessage))
+			{
+				Debug.Log(stringMessage.value);
+			}
+		}
 	}
 }
