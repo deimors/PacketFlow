f6255435fe87ff35b6023b2b6e9e68e9f71f5946
Taylor Fox <TaylorF@iqmetrix.com>
2018-01-27 13:49:03 -0600

    add a shell of a mapper

---
+++ Assets/Code/Processing/ThingToPacketFlowMessageMapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Code.Processing
+{
+	public class ThingToPacketFlowMessageMapper
+	{
+		private const int SAMPLE_MESSAGE_TYPE_ID = -99;
+		private readonly Dictionary<int, Type> payloadTypeLookup = new Dictionary<int, Type> { { SAMPLE_MESSAGE_TYPE_ID, typeof(int) } };
+
+		public PacketFlowMessage Map(object thing)
+		{
+			return new PacketFlowMessage()
+			{
+				senderID = -99,
+				senderType = -99,
+				payloadType = SAMPLE_MESSAGE_TYPE_ID,
+				payload = JsonUtility.ToJson(thing)
+			};
+		}
+
+		public object Map(PacketFlowMessage message)
+		{
+			return JsonUtility.FromJson(message.payload, payloadTypeLookup[message.payloadType]);
+		}
+
+			
+	}
+}
