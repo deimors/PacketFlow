965d3e7172dccc6cb48cc50033132c0027272051
Ryan Marcotte <ryanm@iqmetrix.com>
2018-01-27 09:33:45 -0600

    server only accepts numkey presses; client only accepts letter presses

--- Assets/Code/KeyPressHandler.cs
+++ Assets/Code/KeyPressHandler.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Assets.Code.Extensions;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.Networking.NetworkSystem;
@@ -22,7 +23,15 @@ public class KeyPressHandler : MonoBehaviour {
 		if (!NetworkManager.IsClientConnected())
 			return;
 
-		foreach (var key in Enum.GetValues(typeof(KeyCode)).Cast<KeyCode>().Where(x => Input.GetKeyDown(x)))
-			NetworkManager.client.Send(888, new StringMessage(key.ToString()));
+		if (NetworkServer.connections.Count > 0) // server has connections, client does not (https://stackoverflow.com/a/41685717)
+		{
+			foreach (var key in Enum.GetValues(typeof(KeyCode)).Cast<KeyCode>().Where(x => x.IsNumber() && Input.GetKeyDown(x)))
+				NetworkManager.client.Send(888, new StringMessage(key.ToString()));
+		}
+		else
+		{
+			foreach (var key in Enum.GetValues(typeof(KeyCode)).Cast<KeyCode>().Where(x => x.IsLetter() && Input.GetKeyDown(x)))
+				NetworkManager.client.Send(888, new StringMessage(key.ToString()));
+		}
 	}
 }
