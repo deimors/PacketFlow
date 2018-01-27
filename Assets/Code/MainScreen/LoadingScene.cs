e219f245be43e5f541b0c6e5f24b8f440d60f52a
Tyler Foraie <33034007+tylerforaie@users.noreply.github.com>
2018-01-26 22:49:53 -0600

    Main Page
    Created Buttons for main page and set them up to load the new scenes

---
+++ Assets/Code/MainScreen/LoadingScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingScene : MonoBehaviour {
+
+    public void HackItFlow ()
+    {
+        SceneManager.LoadScene("HackItFlow");
+    }
+
+    public void PacketFlow()
+    {
+        SceneManager.LoadScene("HackItFlow");
+    }
+}
