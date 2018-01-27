ee7f7c34d47c577d36d9f1cae15eb2894ef1f4b3
Ryan Marcotte <ryanm@iqmetrix.com>
2018-01-26 22:11:29 -0600

    initial push

---
+++ Assets/Code/KeyPressHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class KeyPressHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//foreach (var key in Enum.GetValues(typeof(KeyCode)).Cast<KeyCode>().Where(Input.GetKeyDown))
+			
+		
+	}
+}
