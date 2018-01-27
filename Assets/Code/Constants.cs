5fad0d4c9379057d55f1aaee7025ec0affcf7db2
Ryan Marcotte <ryanm@iqmetrix.com>
2018-01-26 22:11:29 -0600

    basic infrastructure for input processor, server-side network message consumer, and client-side network message consumer

---
+++ Assets/Code/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Code
+{
+	public static class Constants
+	{
+		public const int MESSAGE_TYPE_ID = 888;
+		public const int HACKER_TYPE = 0;
+		public const int ADMIN_TYPE = 1;
+	}
+}
---
+++ Assets/Code/PacketFlowMessageNetworkConsumer.cs
@@ -0,0 +1,42 @@
+﻿using Assets.Code;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
+using static Assets.Code.Constants;
+
+public class PacketFlowMessageNetworkConsumer : MonoBehaviour {
+
+	private readonly ConcurrentQueue<PacketFlowMessage> _messageQueue = new ConcurrentQueue<PacketFlowMessage>();
+	private readonly ServerEventDispatcher _eventDispatcher = new ServerEventDispatcher();
+
+	// Use this for initialization
+	void Start () {
+		NetworkServer.RegisterHandler(MESSAGE_TYPE_ID, networkMessage =>
+		{
+			var message = networkMessage.ReadMessage<PacketFlowMessage>();
+			_messageQueue.Enqueue(message);
+		});
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		while (!_messageQueue.IsEmpty)
+		{
+			PacketFlowMessage message;
+			if (_messageQueue.TryDequeue(out message))
+				_eventDispatcher.Dispatch(message);
+		}
+	}
+}
+
+internal class ServerEventDispatcher
+{
+	public void Dispatch(PacketFlowMessage message)
+	{
+		Debug.Log(message.payload);
+	}
+}
