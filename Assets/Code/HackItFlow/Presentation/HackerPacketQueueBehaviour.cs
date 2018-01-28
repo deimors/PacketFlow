56064c3f12dfd7fed0c6724afe0d58269bc5e09f
Chris Dusyk <chris.dusyk@gmail.com>
2018-01-27 23:22:58 -0600

    Auto stash before merge of "HackerPacketQueue" and "origin/HackerPacketQueue"

--- Assets/Code/HackItFlow/Presentation/HackerPacketQueueBehaviour.cs
+++ Assets/Code/HackItFlow/Presentation/HackerPacketQueueBehaviour.cs
@@ -4,11 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using Assets.Code.HackItFlow.HackerPacketQueue;
+using Zenject;
 
 namespace Assets.Code.HackItFlow.Presentation
 {
 	public class HackerPacketQueueBehaviour : MonoBehaviour
 	{
+		private IHackerPacketQueue _queue;
+
+		[Inject]
+		public void Initialize(IHackerPacketQueue queue)
+		{
+			_queue = queue;
+		}
+
 		/// <summary>
 		/// Called once per frame.
 		/// </summary>
