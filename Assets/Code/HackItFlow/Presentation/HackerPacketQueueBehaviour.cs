852c2138bcfea717b8a911c4aeae9b5837fcc63d
Chris Dusyk <chris.dusyk@gmail.com>
2018-01-27 22:14:36 -0600

    Added shell of monobehaviour for hacker keypress
    Also Unity's bullshit

---
+++ Assets/Code/HackItFlow/Presentation/HackerPacketQueueBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Code.HackItFlow.Presentation
+{
+	public class HackerPacketQueueBehaviour : MonoBehaviour
+	{
+		/// <summary>
+		/// Called once per frame.
+		/// </summary>
+		void Update()
+		{
+			if (Event.current.Equals(Event.KeyboardEvent("space")))
+			{
+				// emit stuff
+			}
+		}
+	}
+}
