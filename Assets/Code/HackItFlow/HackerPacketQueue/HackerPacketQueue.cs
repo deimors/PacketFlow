6fccc3f3dc18e3c61aa844696171d8766cd7069e
Chris Dusyk <chris.dusyk@gmail.com>
2018-01-27 23:39:42 -0600

    Can add packet to hacker queue

--- Assets/Code/HackItFlow/HackerPacketQueue/HackerPacketQueue.cs
+++ Assets/Code/HackItFlow/HackerPacketQueue/HackerPacketQueue.cs
@@ -9,12 +9,23 @@ namespace Assets.Code.HackItFlow.HackerPacketQueue
 {
 	public class HackerPacketQueue : IHackerPacketQueue
 	{
+		private const int MAX_QUEUE_SIZE = 9;	// Queue of 8 and 1 infected packet
+
 		private ReactiveCollection<IHackerPacketQueueItem> _items = new ReactiveCollection<IHackerPacketQueueItem>();
 
 		public IReadOnlyReactiveCollection<IHackerPacketQueueItem> Items => _items;
 
 		public bool TryAddItem(HackerPacketQueueItemType type, HackerPacketQueueItemColour colour)
 		{
+			if (_items.Count >= MAX_QUEUE_SIZE)
+			{
+				return false;
+			}
+
+			int halfSize = _items.Count / 2;
+
+			_items.Insert(halfSize, new HackerPacketQueueItem() { Type = type, Colour = colour });
+
 			return true;
 		}
 	}
