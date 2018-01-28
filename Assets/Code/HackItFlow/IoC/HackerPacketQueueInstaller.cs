8dbcad524e2200c2b01f502ea5e189900fe05b6a
Chris Dusyk <chris.dusyk@gmail.com>
2018-01-27 22:18:22 -0600

    Added queue installer to code and scene

---
+++ Assets/Code/HackItFlow/IoC/HackerPacketQueueInstaller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zenject;
+
+namespace Assets.Code.HackItFlow.IoC
+{
+	public class HackerPacketQueueInstaller : MonoInstaller
+	{
+		public override void InstallBindings()
+		{
+		}
+	}
+}
