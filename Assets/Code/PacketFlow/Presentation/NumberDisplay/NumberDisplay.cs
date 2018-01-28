649bdde231940e04c3fea2a5da586b14b2fa63eb
Kael Dow <kaeldow@gmail.com>
2018-01-28 00:40:54 -0600

    Number display

---
+++ Assets/Code/PacketFlow/Presentation/NumberDisplay/NumberDisplay.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberDisplay : MonoBehaviour
+{
+	[SerializeField]
+	private TextMesh _textMesh;
+
+	[SerializeField]
+	public float Value;
+
+	[SerializeField]
+	Color LowColor, HighColor;
+
+	[SerializeField]
+	float LowThreshhold, HighThreshhold;
+
+	void Update()
+	{
+		_textMesh.text = Value.ToString();
+
+		_textMesh.color = Color.Lerp(LowColor, HighColor, GetInterpolationAmount());
+	}
+
+	private float GetInterpolationAmount()
+		=> (Value - LowThreshhold) / (HighThreshhold - LowThreshhold);
+	
+}
