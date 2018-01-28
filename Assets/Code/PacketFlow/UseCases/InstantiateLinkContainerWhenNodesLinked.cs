201d592a420408eb00eee0331d1bce67d79c4396
Andrew Kuipers <deimors@gmail.com>
2018-01-27 19:16:13 -0600

    Drawing Links between a couple Nodes

---
+++ Assets/Code/PacketFlow/UseCases/InstantiateLinkContainerWhenNodesLinked.cs
@@ -0,0 +1,17 @@
+﻿using PacketFlow.Domain;
+using PacketFlow.Presentation.Link;
+using System;
+using UniRx;
+
+namespace PacketFlow.UseCases
+{
+	public class InstantiateLinkContainerWhenNodesLinked
+	{
+		public InstantiateLinkContainerWhenNodesLinked(IObservable<NetworkEvent> networkEvents, LinkContainer.Factory linkFactory)
+		{
+			networkEvents
+				.OfType<NetworkEvent, NetworkEvent.LinkAdded>()
+				.Subscribe(linkAdded => linkFactory.Create(linkAdded.Link.Id));
+		}
+	}
+}
