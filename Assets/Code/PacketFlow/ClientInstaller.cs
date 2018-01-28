caf65978dad08a16a541feb8e09ce1b7b0bf5f96
Taylor Fox <TaylorF@iqmetrix.com>
2018-01-27 21:40:25 -0600

    andrew's stuff

---
+++ Assets/Code/PacketFlow/ClientInstaller.cs
@@ -0,0 +1,16 @@
+﻿using PacketFlow.Actors;
+using PacketFlow.Domain;
+using Zenject;
+
+namespace PacketFlow.Presentation
+{
+	public class ClientInstaller : MonoInstaller
+	{
+		public override void InstallBindings()
+		{
+			Container
+				.BindInterfacesTo<ActorClientProxy<NetworkEvent, NetworkCommand>>()
+				.AsSingle();
+		}
+	}
+}
