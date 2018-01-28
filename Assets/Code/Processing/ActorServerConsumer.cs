253cdcaea7c610216e7c228523c89e7eefe8367e
Taylor Fox <TaylorF@iqmetrix.com>
2018-01-27 20:06:40 -0600

    stuff

--- Assets/Code/Processing/ActorServerConsumer.cs
+++ Assets/Code/Processing/ActorServerConsumer.cs
@@ -10,52 +10,46 @@ using UnityEngine;
 using UnityEngine.Networking;
 using static Assets.Code.Constants;
 
-	public class ActorServerConsumer : MonoBehaviour, IActorServer<NetworkEvent, NetworkCommand>
+public class ActorServerConsumer : MonoBehaviour, IActorServer<NetworkEvent, NetworkCommand>
+{
+	private readonly ConcurrentQueue<PacketFlowMessage> _messageQueue = new ConcurrentQueue<PacketFlowMessage>();
+
+	private readonly ISubject<NetworkCommand> _commandSubject = new Subject<NetworkCommand>();
+	public IObservable<NetworkCommand> ReceivedCommands => _commandSubject;
+
+	public NetworkManager NetworkManagerInstance;
+
+	public void SendEvent(NetworkEvent @event)
 	{
-		private readonly ConcurrentQueue<PacketFlowMessage> _messageQueue = new ConcurrentQueue<PacketFlowMessage>();
-
-		private readonly ISubject<NetworkCommand> _commandSubject = new Subject<NetworkCommand>();
-		public IObservable<NetworkCommand> ReceivedCommands => _commandSubject;
-
-		public NetworkManager NetworkManagerInstance;
-
-		public void SendEvent(NetworkEvent @event)
-		{
-			var eventToSendAcrossWire = NetworkEventAndPacketFlowMessageBidirectionalMapper.Map(@event);
-			NetworkServer.SendToAll(SERVER_MESSAGE_TYPE_ID, eventToSendAcrossWire);
-		}
-
-		void Update()
-		{
-			if (!NetworkManagerInstance.IsClientConnected())
-				return;
-
-		if (!NetworkManagerInstance.client.handlers.ContainsKey(ADMIN_PLAYER_MESSAGE_TYPE_ID))
-		{
-			NetworkManagerInstance.client.RegisterHandler(ADMIN_PLAYER_MESSAGE_TYPE_ID, networkMessage =>
-			{
-				var message = networkMessage.ReadMessage<PacketFlowMessage>();
-				_messageQueue.Enqueue(message);
-			});
-		}
-
-		if (!NetworkManagerInstance.client.handlers.ContainsKey(HACKER_PLAYER_MESSAGE_TYPE_ID))
-			{
-				NetworkManagerInstance.client.RegisterHandler(HACKER_PLAYER_MESSAGE_TYPE_ID, networkMessage =>
-				{
-					var message = networkMessage.ReadMessage<PacketFlowMessage>();
-					_messageQueue.Enqueue(message);
-				});
-			}
-
-			while (!_messageQueue.IsEmpty)
-			{
-				PacketFlowMessage message;
-				if (_messageQueue.TryDequeue(out message))
-				{
-					var command = new NetworkCommandAndPacketFlowMessageBidirectionalMapper().Map(message);
-					_commandSubject.OnNext(command);
-				}
-			}
-		}		
+		var eventToSendAcrossWire = NetworkEventAndPacketFlowMessageBidirectionalMapper.Map(@event);
+		NetworkServer.SendToAll(SERVER_MESSAGE_TYPE_ID, eventToSendAcrossWire);
 	}
+
+	void Start()
+	{
+		NetworkServer.RegisterHandler(HACKER_PLAYER_MESSAGE_TYPE_ID, networkMessage =>
+		{
+			var message = networkMessage.ReadMessage<PacketFlowMessage>();
+			_messageQueue.Enqueue(message);
+		});
+
+		NetworkServer.RegisterHandler(ADMIN_PLAYER_MESSAGE_TYPE_ID, networkMessage =>
+		{
+			var message = networkMessage.ReadMessage<PacketFlowMessage>();
+			_messageQueue.Enqueue(message);
+		});
+	}
+
+	void Update()
+	{
+		while (!_messageQueue.IsEmpty)
+		{
+			PacketFlowMessage message;
+			if (_messageQueue.TryDequeue(out message))
+			{
+				var command = new NetworkCommandAndPacketFlowMessageBidirectionalMapper().Map(message);
+				_commandSubject.OnNext(command);
+			}
+		}
+	}		
+}
