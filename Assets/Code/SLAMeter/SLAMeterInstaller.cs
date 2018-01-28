8300eb16d5ae2c74ab8d3b6c6630023c5002aad9
Chris Dusyk <chris.dusyk@gmail.com>
2018-01-28 14:29:15 -0600

    Added installer for scene context

---
+++ Assets/Code/SLAMeter/SLAMeterInstaller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zenject;
+
+namespace Assets.Code.SLAMeter
+{
+	public class SLAMeterInstaller : MonoInstaller
+	{
+		public override void InstallBindings()
+		{
+			Container.BindInterfacesAndSelfTo<SLAMeter>().AsSingle();
+		}
+	}
+}
